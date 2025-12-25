53df23d539b835661830aa84baae3ac8de62a061
Shikh <sotimovshixnazar8178@gmail.com>
2025-12-25 22:03:58 +0500

    Add evenSum function to calculate sum of even numbers

---
+++ Shikh.js
@@ -0,0 +1,16 @@
+// 1-misol
+
+// function evenSum(array) {
+// 	let newArray = []
+// 	let sum = 0
+// 	for (const element of array) {
+// 		newArray.push(Math.abs(element))
+// 		if (element % 2 == 0) sum += element * element
+// 	}
+// 	return `Yangi array: [${newArray}] va undagi juft sonlarning kvadratlari yigindisi: ${sum}`
+// }
+
+// console.log(evenSum([-1, 2, 3, 4, 5, -6]))
+
+// 2-misol 
+
